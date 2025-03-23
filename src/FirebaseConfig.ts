52cf7ccbee19345cf4a9a33079b4a681d698849c
andrestobar05 <andrewtobar05@gmail.com>
2025-03-22 19:17:10 -0600

    initialize React project with Vite, TypeScript, and ESLint configuration

---
+++ src/FirebaseConfig.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCIqcZWYgD4AYfq1uXq1310Ko5nYsJhwDY",
+  authDomain: "lab-9---firebase-auth-react.firebaseapp.com",
+  projectId: "lab-9---firebase-auth-react",
+  storageBucket: "lab-9---firebase-auth-react.firebasestorage.app",
+  messagingSenderId: "291568402867",
+  appId: "1:291568402867:web:2d3787bf8074a6c6438bc6"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
